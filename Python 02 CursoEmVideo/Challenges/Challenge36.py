4ff0c8430223f66a48085b3c14752addbbc539e4
Marco Antonio Torquato <marcotorquato97@gmail.com>
2022-12-01 12:43:41 +0100

    create challenge 37 and 38

--- Python 02 CursoEmVideo/Challenges/Challenge36.py
+++ Python 02 CursoEmVideo/Challenges/Challenge36.py
@@ -5,8 +5,8 @@ salaryValue= float(input('What is the value of your salary? '))
 installments= float(input('How many years do you want to pay? ')* 12)
 salaryPercent = float(salaryValue * 0.3)
 
-if houseValue/installments <= salaryPercent :
+if houseValue/installments >= salaryPercent :
     print('Congratulations! The value of the installment does not exceed thirty percent of your salary. Your loan has been approved!')
-else :
+else:
     print ('Unfortunately, the value of the installment exceeds the limit of thirty percent of your salary, you cannot finance this!')
 
---
+++ Python 02 CursoEmVideo/Challenges/Challenge37.py
@@ -0,0 +1,3 @@
+print('=====CHALLENGE 37=====')
+
+
