4b39acdf8d7b5ea61f8f5cc05a68a4cfeae9171c
Marco Antonio Torquato <marcotorquato97@gmail.com>
2022-12-01 15:55:52 +0100

    organic

--- Python 02 CursoEmVideo/Challenges/Challenge37.py
+++ Python 02 CursoEmVideo/Challenges/Challenge37.py
@@ -1,3 +1,16 @@
 print('=====CHALLENGE 37=====')
 
-
+num = int(input('Enter an integer: '))
+print('''Choose one of the bases for conversion:
+[ 1 ] convert to BINARY
+[ 2 ] convert to OCTAL
+[ 3 ] convert to HEZADECIMAL''')
+option = int(input('You option is: '))
+if option == 1:
+    print('{} converted for BINARIO is {}'.format(num,(bin(num)[2:])))
+elif option == 2:
+    print('{} converted for OCTAL is {}'.format(num,(oct(num)[2:])))
+elif option == 3:
+    print('{} converted for EXADECIMAL is {}'.format(num, hex(num)[2:]))
+else:
+    print('Invalid')
