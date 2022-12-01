bc0641e3e293a8137ef7683ccf1ea08972862697
Marco Antonio Torquato <marcotorquato97@gmail.com>
2022-12-01 16:31:08 +0100

    up

--- Python 02 CursoEmVideo/Challenges/Challenge39.py
+++ Python 02 CursoEmVideo/Challenges/Challenge39.py
@@ -2,16 +2,16 @@ print('=====CHALLENGE 39=====')
 
 from datetime import date
 
+year = date.today().year
+born = int(input('Enter the year of your born: '))
+age = year - born
+lack = (18 - age)
+late = (age - 18)
 
-born = float(input('Enter the year of your birth'))
-lack = 18 - born
-late = born - 18
 
-
-
-if date.year - born < 18:
-    print('There are {lack} years left for you to enlist')
-elif date.year - born == 18:
+if age < 18:
+    print('There are {} years left for you to enlist'.format(lack))
+elif age == 18:
     print ('Its time for you to join the army')
 else:
-    print('Passed {late} the date of your enlistment!')
+    print('Passed {} years the date of your enlistment!'.format(late))
