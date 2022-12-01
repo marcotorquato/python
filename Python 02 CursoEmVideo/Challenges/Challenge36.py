14475e8a006ddc09f582e070236ee8d9ae76bcfa
Marco Antonio Torquato <marcotorquato97@gmail.com>
2022-12-01 12:33:59 +0100

    challenge36

--- Python 02 CursoEmVideo/Challenges/Challenge36.py
+++ Python 02 CursoEmVideo/Challenges/Challenge36.py
@@ -1,3 +1,12 @@
 print('=====CHALLENGE 36=====')
 
-    
+houseValue= float(input('What is the value of the house? '))
+salaryValue= float(input('What is the value of your salary? '))
+installments= float(input('How many years do you want to pay? ')* 12)
+salaryPercent = float(salaryValue * 0.3)
+
+if houseValue/installments <= salaryPercent :
+    print('Congratulations! The value of the installment does not exceed thirty percent of your salary. Your loan has been approved!')
+else :
+    print ('Unfortunately, the value of the installment exceeds the limit of thirty percent of your salary, you cannot finance this!')
+
