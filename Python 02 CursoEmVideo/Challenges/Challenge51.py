bafbe9d54e04f16f75baf5fb0d3ca4f7563a3c7a
Marco Antonio Torquato <marcotorquato97@gmail.com>
2022-12-05 10:54:55 +0100

    challenge pa

--- Python 02 CursoEmVideo/Challenges/Challenge51.py
+++ Python 02 CursoEmVideo/Challenges/Challenge51.py
@@ -3,7 +3,7 @@ print('=====CHALLENGE 51=====')
 
 start = int(input('Enter the reason for the P.A.: '))
 step = int(input('Enter the reason for the P.A.: '))
-end = start + (10 - 1) * step
+exit = start + (10 - 1) * step
 
-for c in range(start, end + 1, step):
+for c in range(start, exit + 1, step):
     print('-> {}'.format(c))
