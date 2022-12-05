20e97e0b5473ef7f8eebabb78283b885f098d51a
Marco Antonio Torquato <marcotorquato97@gmail.com>
2022-12-05 18:55:34 +0100

    create 59

---
+++ Python 02 CursoEmVideo/Challenges/Challenge59.py
@@ -0,0 +1,36 @@
+print('=====CHALLENGE 59=====')
+from time import sleep 
+
+a = int(input('Write a number: '))
+b = int(input('Write a number: '))
+option = 0
+
+while option.upper != 5 and option in 'SN':
+    print('''  [1] Sum
+    [2] Multiply
+    [3] Bigger
+    [4] Insert new numbers
+    [5] Exit the program.''')
+    option = int(input('What is your option?\n '))
+
+    if option == 1 :
+        print(f'The sum of {a} + {b} is: {a+b}')
+    elif option == 2:
+        print(f'The multiplication of {a} * {b} is: {a*b}')
+    elif option == 3:
+        if a > b:
+            print(f'The first number is {a} and is greater than the second number {b}')
+        elif a < b:
+            print(f'The second number is {b} and is greater than the first {a}')
+        else:
+            print('The numbers are the same')
+    elif option == 4:
+        a = int(input('Write a number: '))
+        b = int(input('Write a number: '))
+    elif option == 5:
+        break
+    else:
+        print('No option! Type again.')
+        print('-=' * 50)
+        sleep(1.0)
+print('You left the program! Until later!')
