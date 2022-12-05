2bcfd7ae5c59f1a070504d56c67c18cf78f0d68b
Marco Antonio Torquato <marcotorquato97@gmail.com>
2022-12-05 16:13:29 +0100

    challenge 56

--- Python 02 CursoEmVideo/Challenges/Challenge56.py
+++ Python 02 CursoEmVideo/Challenges/Challenge56.py
@@ -1,22 +1,27 @@
 print('=====CHALLENGE 56=====')
 
-sum = 0
-cont = 0
-constm = 0
+sumAge = 0
+media = 0
+largerAgeMan = 0 
 oldName = ''
-olgAge = 0
+underWoman20 = 0
 
-for c in range(1, 5):
-    name = input('Digite um name: ')
+for p in range(1, 5):
+    print('------- %d PERSON -------' %(p))
+    name = str(input('Name: ')).strip()    
     age = int(input('Age: '))
-    cont += 1  # this counter will be used to count the variable to average.   kk
-    sum = sum + age # sum to average the age of the group.
-    sex = input('sex [M/F]: ')
-    if sex in 'Mm' and age > olgAge: # check if you are a man and if your age is greater than the initial age and the others.
-        olgAge = age
+    sex = str(input('Sex [M/F]: ')).strip()
+    sumAge += age
+    if p == 1 and sex in 'Mm':
+        oldNameman = age
         oldName = name
-    if sex in 'Ff' and age < 20:
-        constm += 1
-print(f'Average ages {sum / cont}.') # F-String print(f'{example}') replaces .format() in python3.6
-print(f'The oldest man is named {oldName} and is {olgAge} years old.')
-print(f'There are {constm} female(s) younger than 20.')
+    if sex in 'Mm' and age > largerAgeMan:
+        largerAgeMan = age
+        oldName = name
+    if sec in 'Ff' and age < 20:
+        underWoman20 += 1
+media = sumAge / 4
+
+print('The media of group is %d'%(media))
+print('The older man has {} and is called {}' .format(largerAgeMan, oldName))
+print('Are %d women under 20 years old' %(underWoman20))
