51c8e5106e905d8b098f800c21c80de64317bead
Marco Antonio Torquato <marcotorquato97@gmail.com>
2022-12-05 10:33:20 +0100

    check

--- Python 02 CursoEmVideo/Challenges/Challenge49.py
+++ Python 02 CursoEmVideo/Challenges/Challenge49.py
@@ -1,6 +1,6 @@
 print('=====CHALLENGE 49=====')
 
-tabuada = int(input('Enter a number: '))
+tab = int(input('Enter a number: '))
 
 for count in range(10):
-    print("%d x %d = %d" % (tabuada, count+1, tabuada*(count+1)) )
+    print("%d x %d = %d" % (tab, count+1, tab*(count+1)) )
