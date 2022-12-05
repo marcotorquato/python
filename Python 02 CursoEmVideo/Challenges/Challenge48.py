deb3d219419b8e8cd9c41908934fbd48fa8997b2
Marco Antonio Torquato <marcotorquato97@gmail.com>
2022-12-05 10:27:15 +0100

    up 49

--- Python 02 CursoEmVideo/Challenges/Challenge48.py
+++ Python 02 CursoEmVideo/Challenges/Challenge48.py
@@ -2,8 +2,8 @@ print('=====CHALLENGE 48=====')
 
 sum = 0
 
-for c in range(1, 500):
+for c in range(1, 501):
     if c % 2 != 0 and c % 3 == 0:
         sum += c
-        print(c)
+        print(c, end=' ')
 print(sum)
