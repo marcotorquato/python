cd28c1d80526d4e91fec9c1d3fdebb0787270476
Marco Antonio Torquato <marcotorquato97@gmail.com>
2022-12-07 21:30:04 +0100

    up 61

--- Python 02 CursoEmVideo/Challenges/Challenge61.py
+++ Python 02 CursoEmVideo/Challenges/Challenge61.py
@@ -1,1 +1,15 @@
 print('=====CHALLENGE 61=====')
+
+
+start = int(input('Enter the start number: '))
+step = int(input('Enter the step number: '))
+exit = start + (10 - 1) * step
+
+therm = 1
+cont = 0
+while cont <= 10:
+    print('%d = ' %therm)
+    therm += step
+    cont += 1
+print('END')
+
