1dfac4851aaff3690f8b6edca4ec1c2aa0d1a570
Marco Antonio Torquato <marcotorquato97@gmail.com>
2022-12-08 09:23:54 +0100

    64 check

--- Python 02 CursoEmVideo/Challenges/Challenge64.py
+++ Python 02 CursoEmVideo/Challenges/Challenge64.py
@@ -1,11 +1,14 @@
 print('=====CHALLENGE 64=====')
-num = 1
+num = 0
 count = 0
+sum = 0
+num = int(input('[999 to STOP] or Write a number: '))
+
 while num != 999:
-  num = int(input('[999 to STOP] or Write a number: '))
-  num += num
   count +=1
-  total = num  
-print('were entered . numbers and their sum is:')
+  sum += num
+  num = int(input('[999 to STOP] or Write a number: '))
+
+print('Were entered %d numbers and their sum is: %d'%(count,sum))
 
 
