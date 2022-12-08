0e92f9aa9e1f14abfe2c7ff21798086581f1ac07
Marco Antonio Torquato <marcotorquato97@gmail.com>
2022-12-08 09:13:05 +0100

    create 64

---
+++ Python 02 CursoEmVideo/Challenges/Challenge64.py
@@ -0,0 +1,11 @@
+print('=====CHALLENGE 64=====')
+num = 1
+count = 0
+while num != 999:
+  num = int(input('[999 to STOP] or Write a number: '))
+  num += num
+  count +=1
+  total = num  
+print('were entered . numbers and their sum is:')
+
+
