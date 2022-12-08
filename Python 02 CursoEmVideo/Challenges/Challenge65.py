b94c8adcc17ec5a8de488b728c7a0e8d2663fd65
Marco Antonio Torquato <marcotorquato97@gmail.com>
2022-12-08 10:15:27 +0100

    Update Challenge65.py

--- Python 02 CursoEmVideo/Challenges/Challenge65.py
+++ Python 02 CursoEmVideo/Challenges/Challenge65.py
@@ -1,1 +1,22 @@
 print('=====CHALLENGE 65=====')
+
+answer = 'S'
+sum = amount = media = 0
+
+while answer in 'Ss':
+    num = int(input('Write a number: '))
+    sum += num
+    amount += 1
+    if amount == 1:
+        larger = smaller = num 
+    else:
+        if num > larger:
+            larger = num
+        if num < smaller:
+            smaller = num
+
+    answer = str(input('Continue? [S/N] ')).upper().strip()[0]
+
+media = (sum/amount)
+print('You typed %d numbers and the average is: %d.'%(amount,media))
+print('The largest value entered was %d and the smallest was %d'%(larger,smaller))
