5d080cff7a62625e55a444640ddc7edacd818b17
Marco Antonio Torquato <marcotorquato97@gmail.com>
2022-12-08 17:57:54 +0100

    66 check

--- Python 02 CursoEmVideo/Challenges/Challenge66.py
+++ Python 02 CursoEmVideo/Challenges/Challenge66.py
@@ -1,2 +1,11 @@
 print('=====CHALLENGE 66=====')
 
+num = count = s = 0
+count 
+while True:
+    num = int(input('Write a number: '))
+    if num == 999:
+        break
+    count += 1
+    s += num
+print(f' Sum is {s} and {count} numbers were entered')
