56d5fb1e0fac0deaa6587c035de070f59325d438
Marco Antonio Torquato <marcotorquato97@gmail.com>
2022-12-08 18:30:36 +0100

    67 check

--- Python 02 CursoEmVideo/Challenges/Challenge67.py
+++ Python 02 CursoEmVideo/Challenges/Challenge67.py
@@ -1,1 +1,9 @@
 print('=====CHALLENGE 67=====')
+
+while True:
+    tab = int(input('Write a number: '))
+    if tab < 0:
+        break
+
+    for c in range(1,11):
+        print(f'{tab} x {c} = {tab * c}')
