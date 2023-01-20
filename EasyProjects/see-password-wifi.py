cca558e41a25329a9e7c80653edb68f585521f04
Marco Antonio Torquato <marcotorquato97@gmail.com>
2023-01-20 10:55:50 +0100

    Update see-password-wifi.py

--- EasyProjects/see-password-wifi.py
+++ EasyProjects/see-password-wifi.py
@@ -19,3 +19,24 @@ for i in data:
 
         profiles.append(i)
 
+print("{:<30} | {:<}".format("WI-FI Name","Password"))
+print("-------------------------------------------------------")
+
+for i in profiles:
+
+    try:
+        results = subprocess.check_output(['netsh', 'wlan','profile', i, 'key = clear'])
+
+        results = results.decode('utf-8', errors = "backslashreplace")
+        results = results.split('\n')
+
+        results = [b.split(":") for b in results if  "key Content" in b] 
+
+        try:
+            print("{:<30} | {:<}".format(i, results[0]))
+
+            except indexError:
+                print("{:<30} | {:<}".format(i, ""))
+    
+    except subprocess.CalledProcessError:
+        print("Ecoding Error Occured")
