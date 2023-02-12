028b4f460eb9b38100a5809a578edd847702a20a
Marco Antonio Torquato <marcotorquato97@gmail.com>
2023-02-12 15:42:32 +0100

    Update automat-geoclick.py

--- EasyProjects/geoclick/automat-geoclick.py
+++ EasyProjects/geoclick/automat-geoclick.py
@@ -10,4 +10,23 @@ pyautogui.write('Marco',duration=2)
 
 with open('produtos.txt', 'r') as file:
     for line in file:
-        line.split(,)
+        line.split(,)[0]
+        product = line.split(',')[1]
+        unit = line..split(',')[2]
+        price = line.split(',')[3]
+
+        #click and edit product
+        pyautogui.click(259,298,duration=2)
+        pyautogui.write(product)
+
+        #click and edit unit
+        pyautogui.click(259,298,duration=2)
+        pyautogui.write(unit)
+
+        #click and edit price
+        pyautogui.click(259,298,duration=2)
+        pyautogui.write(price)
+
+        #button register
+        pyautogui.click(907,231,duration=2)
+        sleep(1)
