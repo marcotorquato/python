0cf73598d4e8f2207ac08c22b5ef70c2e2e17490
Marco Antonio Torquato <marcotorquato97@gmail.com>
2023-02-12 14:14:55 +0100

    up

--- EasyProjects/geoclick/automat-geoclick.py
+++ EasyProjects/geoclick/automat-geoclick.py
@@ -1,3 +1,13 @@
 import pyautogui
 from time import sleep
 
+
+
+pyautogui.click(920,869,duration=1.5) 
+pyautogui.write('Marco',duration=2)
+
+#open file 
+
+with open('produtos.txt', 'r') as file:
+    for line in file:
+        line.split(,)
